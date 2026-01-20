b1ce587ee1903471d408b196141ea8bcb19cd9d1
comkar893 <comkar893@gmail.com>
2026-01-19 22:06:10 -0500

    Try this again

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  assetPrefix: "/comkar893.github.io",
+  assetPrefix: "/",
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
