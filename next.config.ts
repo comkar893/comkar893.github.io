0115812d792c12b80d011c172fb360343a955034
comkar893 <comkar893@gmail.com>
2026-01-19 21:56:28 -0500

    Try this

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  assetPrefix: "./",
+  assetPrefix: "https://github.com/comkar893/comkar893.github.io/tree/gh-pages",
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
